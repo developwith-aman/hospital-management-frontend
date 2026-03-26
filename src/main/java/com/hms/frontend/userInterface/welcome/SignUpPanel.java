9bb323678bd906078f278fedab7db97764c099de
Aman Singh <developwithaman@gmail.com>
2026-03-27 01:11:12 +0530

    Create Sign-Up panel UI for user authentication

--- src/main/java/com/hms/frontend/userInterface/welcome/SignUpPanel.java
+++ src/main/java/com/hms/frontend/userInterface/welcome/SignUpPanel.java
@@ -1,2 +1,14 @@
-package com.hms.frontend.userInterface.welcome;public class SignUpPanel {
+package com.hms.frontend.userInterface.welcome;
+
+import com.hms.frontend.userInterface.MainFrame;
+
+import javax.swing.*;
+
+public class SignUpPanel extends JPanel {
+
+    private MainFrame mainFrame;
+
+    public SignUpPanel(MainFrame mainFrame) {
+        this.mainFrame = mainFrame;
+    }
 }
