222f95232375156f9223f196564e2eb8d0270c96
Aman Singh <developwithaman@gmail.com>
2026-03-27 01:14:31 +0530

    Create Patient dashboard

--- src/main/java/com/hms/frontend/userInterface/dashboards/PatientDashboard.java
+++ src/main/java/com/hms/frontend/userInterface/dashboards/PatientDashboard.java
@@ -1,2 +1,20 @@
-package com.hms.frontend.userInterface.dashboards;public class PatientDashboard {
+package com.hms.frontend.userInterface.dashboards;
+
+import com.hms.frontend.userInterface.MainFrame;
+
+import javax.swing.*;
+
+public class PatientDashboard extends JPanel {
+
+    private MainFrame mainFrame;
+
+    public PatientDashboard(MainFrame mainFrame) {
+        this.mainFrame = mainFrame;
+
+        setLayout(null);
+
+        JLabel label = new JLabel("Welcome to Patient Dashboard");
+        label.setBounds(300, 200, 200, 30);
+        add(label);
+    }
 }
