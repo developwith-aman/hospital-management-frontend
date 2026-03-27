df3364666aaf76a09c72d011cc9b04cd11b86a0f
Aman Singh <developwithaman@gmail.com>
2026-03-27 13:25:15 +0530

    Add SignUpResponseDTO

--- src/main/java/com/hms/frontend/dto/signup/SignUpResponseDTO.java
+++ src/main/java/com/hms/frontend/dto/signup/SignUpResponseDTO.java
@@ -1,2 +1,16 @@
-package com.hms.frontend.dto.signup;public class SignUpResponseDTO {
+package com.hms.frontend.dto.signup;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SignUpResponseDTO {
+
+    private Long userId;
+    private String username;
 }
