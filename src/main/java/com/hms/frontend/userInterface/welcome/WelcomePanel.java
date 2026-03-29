57d7d012558a06568e384616516a656745b1a670
Aman Singh <developwithaman@gmail.com>
2026-03-29 22:56:33 +0530

    Add Exit Button in Welcome Panel

--- src/main/java/com/hms/frontend/userInterface/welcome/WelcomePanel.java
+++ src/main/java/com/hms/frontend/userInterface/welcome/WelcomePanel.java
@@ -18,30 +18,41 @@ public class WelcomePanel extends JPanel {
         add(loginHeader);
 
         JButton goLoginBtn = new JButton("Login");
-        goLoginBtn.setBounds(170, 200, 100, 30);
+        goLoginBtn.setFocusPainted(false);
+        goLoginBtn.setBounds(180, 200, 100, 30);
         goLoginBtn.setBackground(new Color(100, 149, 237)); // Cornflower blue
         goLoginBtn.setForeground(Color.WHITE);
-        goLoginBtn.setFont(new Font("Tahoma", Font.BOLD, 15));
+        goLoginBtn.setFont(new Font("Tahoma", Font.BOLD, 18));
         add(goLoginBtn);
 
         goLoginBtn.addActionListener(e -> mainFrame.showLogin());
 
         // ===== SIGNUP PROMPT =====
         JLabel signupHeader = new JLabel("New user? Create an account below");
-        signupHeader.setBounds(400, 150, 300, 30);
+        signupHeader.setBounds(410, 150, 300, 30);
         signupHeader.setFont(new Font("Tahoma", Font.BOLD, 14));
         add(signupHeader);
 
         JButton goSignupBtn = new JButton("Signup");
-        goSignupBtn.setBounds(470, 200, 100, 30);
+        goSignupBtn.setBounds(480, 200, 100, 30);
         goSignupBtn.setBackground(new Color(34, 139, 34)); // Forest green
         goSignupBtn.setForeground(Color.WHITE);
-        goSignupBtn.setFont(new Font("Tahoma", Font.BOLD, 15));
+        goSignupBtn.setFont(new Font("Tahoma", Font.BOLD, 18));
         add(goSignupBtn);
 
-        setBackground(Color.CYAN);
-
         goSignupBtn.addActionListener(e -> mainFrame.showSignup());
+
+        // Exit Button
+        JButton goExitBtn = new JButton("EXIT");
+        goExitBtn.setBounds(330, 270, 100, 30);
+        goExitBtn.setBackground(Color.RED);
+        goExitBtn.setForeground(Color.white);
+        goExitBtn.setFont(new Font("Tahoma", Font.BOLD, 20));
+        add(goExitBtn);
+
+        goExitBtn.addActionListener(e -> System.exit(0));
+
+        setBackground(Color.CYAN);
     }
 
 }
