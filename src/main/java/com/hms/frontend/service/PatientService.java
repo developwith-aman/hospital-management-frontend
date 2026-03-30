a074db276498b8ea6911c9f38837b2ee26e476d3
Aman Singh <developwithaman@gmail.com>
2026-03-30 19:56:29 +0530

    Temporarily removed the Appointment list feature

--- src/main/java/com/hms/frontend/service/PatientService.java
+++ src/main/java/com/hms/frontend/service/PatientService.java
@@ -1,6 +1,8 @@
 package com.hms.frontend.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.hms.frontend.dto.patient.PatientAppointmentsDTO;
 import com.hms.frontend.dto.patient.PatientsDTO;
 import com.hms.frontend.utils.ApiUtil;
@@ -33,6 +35,8 @@ public class PatientService {
             br.close();
 
             ObjectMapper mapper = new ObjectMapper();
+            mapper.registerModule(new JavaTimeModule());
+            mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
             return mapper.readValue(response.toString(), PatientsDTO.class);
 
         } catch (Exception e) {
@@ -41,30 +45,30 @@ public class PatientService {
         return null;
     }
 
-    public List<PatientAppointmentsDTO> getAppointments(Long userId) {
-        try {
-            URL url = new URL("http://localhost:8080/patients/appointments/patientId/" + userId);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-
-            // Add JWT header
-            ApiUtil.addJwtHeader(conn);
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            StringBuilder response = new StringBuilder();
-            String line;
-            while ((line = br.readLine()) != null) {
-                response.append(line);
-            }
-            br.close();
-
-            ObjectMapper mapper = new ObjectMapper();
-            PatientAppointmentsDTO[] arr = mapper.readValue(response.toString(), PatientAppointmentsDTO[].class);
-            return Arrays.asList(arr);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+//    public List<PatientAppointmentsDTO> getAppointments(Long userId) {
+//        try {
+//            URL url = new URL("http://localhost:8080/patients/appointments/patientId/" + userId);
+//            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+//            conn.setRequestMethod("GET");
+//
+//            // Add JWT header
+//            ApiUtil.addJwtHeader(conn);
+//
+//            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+//            StringBuilder response = new StringBuilder();
+//            String line;
+//            while ((line = br.readLine()) != null) {
+//                response.append(line);
+//            }
+//            br.close();
+//
+//            ObjectMapper mapper = new ObjectMapper();
+//            PatientAppointmentsDTO[] arr = mapper.readValue(response.toString(), PatientAppointmentsDTO[].class);
+//            return Arrays.asList(arr);
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
 }
