b5c05236b369696a35900661fee65c6f57511830
Aman Singh <developwithaman@gmail.com>
2026-04-01 23:53:30 +0530

    Add DoctorPanel UI with doctor-related actions

--- src/main/java/com/hms/frontend/userInterface/panels/DoctorPanel.java
+++ src/main/java/com/hms/frontend/userInterface/panels/DoctorPanel.java
@@ -1,2 +1,24 @@
-package com.hms.frontend.userInterface.panels;public class DoctorPanel {
+package com.hms.frontend.userInterface.panels;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class DoctorPanel extends JPanel {
+
+    public DoctorPanel() {
+
+        setLayout(new GridLayout(2, 2, 15, 15));
+        setBackground(Color.WHITE);
+        add(createButton("Add Doctor"));
+        add(createButton("Assign Appointment"));
+        add(createButton("View Appointments"));
+    }
+
+    private JButton createButton(String text) {
+        JButton button = new JButton(text);
+        button.setFont(new Font("Impact", Font.PLAIN, 25));
+        button.setBackground(new Color(70,150,255));
+        button.setForeground(Color.WHITE);
+        return button;
+    }
 }
