695f6a138707228c800100dd8df8f2eea444b999
Aman Singh <developwithaman@gmail.com>
2026-04-02 22:53:54 +0530

    Create insurance dto for patient registration form

--- src/main/java/com/hms/frontend/dto/InsuranceDTO.java
+++ src/main/java/com/hms/frontend/dto/InsuranceDTO.java
@@ -1,2 +1,19 @@
-package com.hms.frontend.dto;public class InsuranceDTO {
+package com.hms.frontend.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class InsuranceDTO {
+
+    private String policyNumber;
+    private String policyProvider;
+    private LocalDateTime effectiveDate;
+    private LocalDateTime expiryDate;
+
 }
