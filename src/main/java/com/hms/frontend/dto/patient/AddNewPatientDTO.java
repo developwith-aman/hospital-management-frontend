e9861e266cbb89f5e60d5b6dc37f1681c1b0e6a5
Aman Singh <developwithaman@gmail.com>
2026-04-02 22:48:25 +0530

    Create PatientDTO to add a new patient

--- src/main/java/com/hms/frontend/dto/patient/AddNewPatientDTO.java
+++ src/main/java/com/hms/frontend/dto/patient/AddNewPatientDTO.java
@@ -1,2 +1,20 @@
-package com.hms.frontend.dto.patient;public class AddNewPatientDTO {
+package com.hms.frontend.dto.patient;
+
+
+import com.hms.frontend.dto.InsuranceDTO;
+import com.hms.frontend.dto.enums.BloodGroups;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AddNewPatientDTO {
+
+    private String patientName;
+    private String gender;
+    private int age;
+    private String email;
+    private BloodGroups bloodGroup;
+    private InsuranceDTO insurance;
+    private double payableAmount;
 }
