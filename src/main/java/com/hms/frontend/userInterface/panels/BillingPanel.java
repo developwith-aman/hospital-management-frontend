ef151600431d9963f0fe64730bbb251e151e056f
Aman Singh <developwithaman@gmail.com>
2026-04-02 12:03:06 +0530

    Add Billing Panel UI for Billing related Services

--- src/main/java/com/hms/frontend/userInterface/panels/BillingPanel.java
+++ src/main/java/com/hms/frontend/userInterface/panels/BillingPanel.java
@@ -1,2 +1,25 @@
-package com.hms.frontend.userInterface.panels;public class BillingPanel {
+package com.hms.frontend.userInterface.panels;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class BillingPanel extends JPanel {
+
+    public BillingPanel() {
+
+        setLayout(new FlowLayout(FlowLayout.CENTER, 20, 20));
+        setBackground(Color.WHITE);
+
+        add(createButton("Generate Bill"));
+    }
+
+    private JButton createButton(String text) {
+        JButton button = new JButton(text);
+        button.setFont(new Font("Impact", Font.PLAIN, 25));
+        button.setBackground(new Color(0, 128, 128)); // Teal
+        button.setForeground(Color.WHITE);
+        button.setFocusPainted(false);
+        button.setPreferredSize(new Dimension(250, 50));
+        return button;
+    }
 }
