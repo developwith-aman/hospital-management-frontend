456d91913f2c51574c993fdbda70c2b290fb012f
Aman Singh <developwithaman@gmail.com>
2026-04-02 19:19:06 +0530

    Refactor: Shift Api calling logic to ApiClient class

--- src/main/java/com/hms/frontend/service/PatientService.java
+++ src/main/java/com/hms/frontend/service/PatientService.java
@@ -3,6 +3,7 @@ package com.hms.frontend.service;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.hms.frontend.api.ApiClient;
 import com.hms.frontend.dto.patient.PatientsDTO;
 import com.hms.frontend.utils.ApiUtil;
 
@@ -15,57 +16,10 @@ public class PatientService {
     private ObjectMapper mapper = new ObjectMapper();
 
     public PatientsDTO getPatientDetails(Long patientId) {
-        try {
-            URL url = new URL("http://localhost:8080/patients/fetch/patient-details/" + patientId);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
 
-            // Add JWT header
-            ApiUtil.addJwtHeader(conn);
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            StringBuilder response = new StringBuilder();
-            String line;
-            while ((line = br.readLine()) != null) {
-                response.append(line);
-            }
-            br.close();
-
-            ObjectMapper mapper = new ObjectMapper();
-            mapper.registerModule(new JavaTimeModule());
-            mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-            return mapper.readValue(response.toString(), PatientsDTO.class);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
+        return ApiClient.getWithToken(
+                "/patients/fetch/patient-details/" + patientId,
+                PatientsDTO.class
+        );
     }
-
-//    public List<PatientAppointmentsDTO> getAppointments(Long userId) {
-//        try {
-//            URL url = new URL("http://localhost:8080/patients/appointments/patientId/" + userId);
-//            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-//            conn.setRequestMethod("GET");
-//
-//            // Add JWT header
-//            ApiUtil.addJwtHeader(conn);
-//
-//            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-//            StringBuilder response = new StringBuilder();
-//            String line;
-//            while ((line = br.readLine()) != null) {
-//                response.append(line);
-//            }
-//            br.close();
-//
-//            ObjectMapper mapper = new ObjectMapper();
-//            PatientAppointmentsDTO[] arr = mapper.readValue(response.toString(), PatientAppointmentsDTO[].class);
-//            return Arrays.asList(arr);
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        return null;
-//    }
 }
