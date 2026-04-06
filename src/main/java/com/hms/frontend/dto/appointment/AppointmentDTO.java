9ad8391d12f71b60c934565ced4dd841d8e0adf4
Aman Singh <developwithaman@gmail.com>
2026-04-06 17:29:04 +0530

    Create Appointment dto

--- src/main/java/com/hms/frontend/dto/appointment/AppointmentDTO.java
+++ src/main/java/com/hms/frontend/dto/appointment/AppointmentDTO.java
@@ -1,2 +1,24 @@
-package com.hms.frontend.dto.appointment;public class AppointmentDTO {
+package com.hms.frontend.dto.appointment;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class AppointmentDTO {
+
+    private Long appointment_ID;
+    private int doctorId;
+    private Long patientId;
+    private LocalDateTime appointment_time;
+    private String reason;
 }
