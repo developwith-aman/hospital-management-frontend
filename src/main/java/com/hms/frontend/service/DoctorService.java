567f7f23efc0e74790aae39298c272d14b0dd98b
Aman Singh <developwithaman@gmail.com>
2026-04-08 22:29:53 +0530

    feat: implement DoctorService for backend API communication

--- src/main/java/com/hms/frontend/service/DoctorService.java
+++ src/main/java/com/hms/frontend/service/DoctorService.java
@@ -1,2 +1,32 @@
-package com.hms.frontend.service;public class doctorService {
+package com.hms.frontend.service;
+
+import com.hms.frontend.api.ApiClient;
+import com.hms.frontend.dto.appointment.AppointmentDTO;
+import com.hms.frontend.dto.doctor.AddNewDoctorDTO;
+import com.hms.frontend.dto.doctor.DoctorDTO;
+
+
+public class DoctorService {
+
+    public DoctorDTO addNewDoctor(AddNewDoctorDTO newDoctorDTO) {
+        return ApiClient.postWithToken(
+                "/doctors/add",
+                newDoctorDTO,
+                DoctorDTO.class
+        );
+    }
+
+    public AppointmentDTO reassignAppointment(int doctorId, Long appointmentId) {
+
+        String endpoint = String.format("/doctors/reassign-appointment/%d/%d", doctorId, appointmentId);
+        try {
+            return ApiClient.patchWithToken(
+                    endpoint,
+                    null,
+                    AppointmentDTO.class
+            );
+        }catch (Exception e) {
+            throw new IllegalArgumentException("Invalid ID entered");
+        }
+    }
 }
