0a500f090c5a770d347b50ccf9edca00c067e435
Aman Singh <developwithaman@gmail.com>
2026-04-08 22:33:06 +0530

    Create a Doctor dto for safe data transfer

--- src/main/java/com/hms/frontend/dto/doctor/DoctorDTO.java
+++ src/main/java/com/hms/frontend/dto/doctor/DoctorDTO.java
@@ -1,2 +1,16 @@
-package com.hms.frontend.dto.doctor;public class DoctorDTO {
+package com.hms.frontend.dto.doctor;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DoctorDTO {
+
+    private int ID;
+    private String doctorName;
+    private String specialization;
+    private String email;
+    private double consultationFee;
 }
