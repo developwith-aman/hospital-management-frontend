815f6be22325aac5c8afae8c5b86a42e58a355f4
Aman Singh <developwithaman@gmail.com>
2026-04-10 00:07:48 +0530

    Create a new service for department related service work

--- src/main/java/com/hms/frontend/service/DepartmentService.java
+++ src/main/java/com/hms/frontend/service/DepartmentService.java
@@ -1,2 +1,17 @@
-package com.hms.frontend.service;public class DepartmentService {
+package com.hms.frontend.service;
+
+import com.hms.frontend.api.ApiClient;
+import com.hms.frontend.dto.department.AddNewDepartmentDTO;
+import com.hms.frontend.dto.department.DepartmentDTO;
+
+public class DepartmentService {
+
+    public DepartmentDTO addNewDepartment(AddNewDepartmentDTO newDepartmentDTO) {
+
+        return ApiClient.postWithToken(
+                "/department/add",
+                newDepartmentDTO,
+                DepartmentDTO.class
+        );
+    }
 }
