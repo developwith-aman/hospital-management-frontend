83920aa3e13f51054bcf165d1ba5a0eab20bddcc
Aman Singh <developwithaman@gmail.com>
2026-04-10 00:06:17 +0530

    Create a dto for adding new department

--- src/main/java/com/hms/frontend/dto/department/AddNewDepartmentDTO.java
+++ src/main/java/com/hms/frontend/dto/department/AddNewDepartmentDTO.java
@@ -1,2 +1,17 @@
-package com.hms.frontend.dto.department;public class AddNewDepartmentDTO {
+package com.hms.frontend.dto.department;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddNewDepartmentDTO {
+
+    private String departmentName;
+    private int headDoctorId;
+
 }
