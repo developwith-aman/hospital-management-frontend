d0b8cc24776a89d042e4c045e37a62d271412325
Aman Singh <developwithaman@gmail.com>
2026-04-11 00:29:52 +0530

    Create request methods for adding, viewing, and deleting staff in service

--- src/main/java/com/hms/frontend/service/StaffService.java
+++ src/main/java/com/hms/frontend/service/StaffService.java
@@ -1,2 +1,53 @@
-package com.hms.frontend.service;public class StaffService {
+package com.hms.frontend.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hms.frontend.api.ApiClient;
+import com.hms.frontend.api.ApiResponse;
+import com.hms.frontend.dto.staff.AddNewStaffMember;
+import com.hms.frontend.dto.staff.StaffDTO;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class StaffService {
+    private ObjectMapper mapper = new ObjectMapper();
+
+    public StaffDTO addNewStaffMember(AddNewStaffMember newStaffMember) {
+
+        return ApiClient.postWithToken(
+                "/hospital/staff/add",
+                newStaffMember,
+                StaffDTO.class
+        );
+    }
+
+    public List<StaffDTO> fetchAllStaff() {
+
+        StaffDTO[] staffArray = ApiClient.getWithToken(
+                "/hospital/staff/fetch",
+                StaffDTO[].class
+        );
+        if (staffArray != null) {
+            return Arrays.asList(staffArray);
+        }
+        return new ArrayList<>();
+    }
+
+    public ApiResponse deleteStaffMember(Long id) {
+
+        ApiResponse response = ApiClient.deleteWithToken(
+                "/hospital/delete/staff/" + id,
+                ApiResponse.class
+        );
+
+        if (response == null) {
+            throw new IllegalArgumentException("Staff member not found or Server Error.");
+        }
+        String msg = response.getResponseMessage().toLowerCase();
+        if (msg.contains("not found") || msg.contains("error")) {
+            throw new IllegalArgumentException(response.getResponseMessage());
+        }
+        return response;
+    }
 }
