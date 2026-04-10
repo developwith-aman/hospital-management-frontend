5c23a426ee3cb7bc908e24a62db494535c7bcd81
Aman Singh <developwithaman@gmail.com>
2026-04-10 13:55:59 +0530

    Create DTO for adding doctors into the department

--- src/main/java/com/hms/frontend/dto/department/DoctorDepartmentDTO.java
+++ src/main/java/com/hms/frontend/dto/department/DoctorDepartmentDTO.java
@@ -1,2 +1,14 @@
-package com.hms.frontend.dto.department;public class DoctorDepartmentDTO {
+package com.hms.frontend.dto.department;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class DoctorDepartmentDTO {
+
+    private Long departmentID;
+    private int doctorID;
 }
